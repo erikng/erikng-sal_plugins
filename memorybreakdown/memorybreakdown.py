05ae416514e1aa80266f01b9de036170f6be70f2
Erik Gomez <e@eriknicolasgomez.com>
2015-09-18 11:32:13 -0500

    Initial commit
    memory breakdown

---
+++ memorybreakdown/memorybreakdown.py
@@ -0,0 +1,53 @@
+from yapsy.IPlugin import IPlugin
+from yapsy.PluginManager import PluginManager
+from django.template import loader, Context
+from django.db.models import Count
+from server.models import *
+from django.shortcuts import get_object_or_404
+import server.utils as utils
+
+class Memory(IPlugin):
+    def plugin_type(self):
+        return 'builtin'
+
+    def show_widget(self, page, machines=None, theid=None):
+        # The data is data is pulled from the database and passed to a template.
+
+        # There are three possible views we're going to be rendering to - front, bu_dashbaord and group_dashboard. If page is set to bu_dashboard, or$
+        if page == 'front':
+            t = loader.get_template('erikng/memorybreakdown/templates/mem_front.html')
+            if not machines:
+                machines = Machine.objects.all()
+
+        if page == 'bu_dashboard':
+            t = loader.get_template('erikng/memorybreakdown/templates/mem_id.html')
+            if not machines:
+                machines = utils.getBUmachines(theid)
+
+        if page == 'group_dashboard':
+            t = loader.get_template('erikng/memorybreakdown/templates/mem_id.html')
+            if not machines:
+                machine_group = get_object_or_404(MachineGroup, pk=theid)
+                machines = Machine.objects.filter(machine_group=machine_group)
+
+        if machines:
+            mem = machines.values('memory').annotate(count=Count('memory')).order_by()
+#             mem = machines.values('memory').count()
+#            mem = machines.filter('memory_kb').count()
+        else:
+            mem = []
+
+        c = Context({
+            'title': 'Memory Breakdown',
+            'data': mem,
+            'theid': theid,
+            'page': page
+        })
+        return t.render(c), 4
+
+    def filter_machines(self, machines, data):
+        # You will be passed a QuerySet of machines, you then need to perform some filtering based on the 'data' part of the url from the show_widget$
+
+        machines = machines.filter(memory__exact=data)
+
+        return machines, 'Install Memory '+data
